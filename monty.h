b83fc41292ee9b01e8a8f245d9374e38381cb075
spaceadh <spacesealadhesives13@gmail.com>
2024-01-27 06:41:54 +0300

    RedoingCorrections

--- monty.h
+++ monty.h
@@ -1,17 +1,24 @@
-#ifndef MONTY_H
-#define MONTY_H
+#ifndef __HOOLB__
+#define __HOOLB__
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/syscall.h>
+#include <string.h>
+
 /**
-* struct stack_s - doubly linked list representation of a stack (or queue)
-* @n: integer
-* @prev: points to the previous element of the stack (or queue)
-* @next: points to the next element of the stack (or queue)
-*
-* Description: doubly linked list node structure
-* for stack, queues, LIFO, FIFO
-*/
-
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
 typedef struct stack_s
 {
 	int n;
@@ -20,39 +27,32 @@ typedef struct stack_s
 } stack_t;
 
 /**
-* struct instruction_s - opcode and its function
-* @opcode: the opcode
-* @f: function to handle the opcode
-*
-* Description: opcode and its function
-* for stack, queues, LIFO, FIFO
-*/
-
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
 typedef struct instruction_s
 {
 	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+	void (*f)(stack_t **stack, unsigned int line_num);
 } instruction_t;
 
-/**
-* prototypes - for used functions
-* @stack: the stack
-* @i: line number
-* @trigger: opcode
-*/
+void pint(stack_t **stack, unsigned int line_num);
+void free_stack(stack_t *head);
+int pushint(char *list, int ln);
+int _strcmp(char *opcode, char *list);
+void add(stack_t **stack, unsigned int line_num);
+void swap(stack_t **stack, unsigned int line_num);
+void pop(stack_t **stack, unsigned int line_num);
+void push(stack_t **stack, unsigned int line_number, int n);
+void pall(stack_t **stack, unsigned int line_num);
+void execute(char *string[], stack_t *stack);
+int nlfind(char *list);
+void free_list(char *a[]);
+int combfind(char *list, int ln);
 
-void _push(stack_t **stack, unsigned int i, char *trigger);
-void _pall(stack_t **stack, unsigned int i);
-void pint(stack_t **stack, unsigned int i);
-void pop(stack_t **stack, unsigned int i);
-void nop(stack_t **stack, unsigned int i);
-void add(stack_t **stack, unsigned int i);
-void swap(stack_t **stack, unsigned int i);
-void _mul(stack_t **stack, unsigned int i);
-void _div(stack_t **stack, unsigned int i);
-void _sub(stack_t **stack, unsigned int i);
-void _mod(stack_t **stack, unsigned int i);
-void pstr(stack_t **stack, unsigned int i);
-void pchar(stack_t **stack, unsigned int i);
-void free_stack(stack_t **stack);
+
 #endif
